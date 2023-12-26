b132a8c49bc9a87dd4d36abf73a2d396766557a3
BishalGupta2001 <guptabishal512@gmail.com>
2023-12-26 22:22:03 +0530

    added local storage

--- main.js
+++ main.js
@@ -4,44 +4,77 @@ let todoArray = [
   // {item: 'complete myntra clone ', dueDate: '31.12.23'},
   // {item: 'do DSA', dueDate: '26.12.23'},
 ];
-displayItem();
 
-function addTodo() { //if button clicked this function will run 
+window.addEventListener("load", function () {
+  let storedData = localStorage.getItem("todoData");
+  //  check if data already exist
+  if (storedData) {
+    // Parse the stored JSON data and update the todoArray
+    todoArray = JSON.parse(storedData);
+    displayItem();
+  }
+});
 
-  let inputElement = document.querySelector("#todo");//selected button
-  let dateElement = document.querySelector("#todo-date");// selected date element 
+//function to  add new todo
+function addTodo() {
+  // Select input elements for todo text and date
+  let inputElement = document.querySelector("#todo");
+  let dateElement = document.querySelector("#todo-date");
 
-  let todoValue = inputElement.value;//got the input value 
+  // Get values from input elements
+  let todoValue = inputElement.value; //got the input value
   let todoDate = dateElement.value;
 
-  todoArray.push({item: todoValue, dueDate: todoDate}); //added in the array
+  // added new object value into array
+  todoArray.push({ item: todoValue, dueDate: todoDate }); //added in the array
 
-  inputElement.value = ""; //input/value will be empty in input tag
-  dateElement.value ="";
+  // clear input values
+  inputElement.value = "";
+  dateElement.value = "";
 
-  displayItem();// then display  this function
+  // set the given data by user in local storage and converted into json
+  localStorage.setItem("todoData", JSON.stringify(todoArray));
+
+  // display the updated todo item
+  displayItem();
 }
 
+// Function to display todo items on the page
 function displayItem() {
-  let todoItem = document.querySelector(".todo-items"); // selected div tag here
-  let newHtml ="";//created a variable with empty value;
+  // selected div tag here
+  let todoItem = document.querySelector(".todo-items");
+
+  //created a variable with empty value;
+  let newHtml = "";
+
   for (let i = 0; i < todoArray.length; i++) {
-    
     // let item =  todoArray[i].item;
     // let dueDate = todoArray[i].dueDate;
-//                 OR
-    let {item,dueDate} = todoArray[i];
+    //                 OR
+    // Destructure the todo object to get item and dueDate
+    let { item, dueDate } = todoArray[i];
 
-    // ye div chalega har ek iteration ma 
+    // ye div chalega har ek iteration ma
     // what we have written span is giving that particular thing
-    // button is deleting that specific  value or item 
-    
+    // button is deleting that specific  value or item
+
     newHtml += ` 
 
     <span> ${item} </span>
     <span> ${dueDate} </span>
-    <button onclick ="todoArray.splice(${i}, 1); displayItem();">Delete</button>`;
+    <button onclick ="deleteTodo(${i})">Delete</button>`;
   }
-  // 
+  //
   todoItem.innerHTML = newHtml;
+}
+
+function deleteTodo(i) {
+  // Remove the todo item at the specified index from the todoArray
+  todoArray.splice(i, 1);
+
+  // Save the updated todoArray to local storage as JSON
+  localStorage.setItem("todoData", JSON.stringify(todoArray));
+
+  // Display the updated todo items on the page
+  displayItem();
 }
